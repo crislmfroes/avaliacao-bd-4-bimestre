dfd4f5a9851c7ab02d693f6eff23239dbc5c91bc
CarolineVeleda <carollimaveleda@yahoo.com.br>
2018-12-05 17:20:07 -0200

    Add files via upload

---
+++ php/controllers/excluirAtividade.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once('../model/AtividadeDao.php');
+include_once('../model/atividade.php');
+
+
+
+$cod = $_POST['codAtividade'];
+$adao = new atividadeDao();
+$atividade=$adao->find(intval($cod));
+
+$adao->delete($atividade);
+//var_dump($t);
+//header('Location: home.php');
+
+
+?>
