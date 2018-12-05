09fd51448137a5c9db709d65790016083b4668c1
CarolineVeleda <carollimaveleda@yahoo.com.br>
2018-12-05 18:57:32 -0200

    Add files via upload

--- php/controllers/inserirAtividade.php
+++ php/controllers/inserirAtividade.php
@@ -3,26 +3,34 @@ include_once('../views/criarAtividade.html');
 include_once('../model/atividadeDao.php');
 include_once('../model/atividade.php');
 
+$cod = $_POST['cod'];
+$a = new Atividade($_POST['nomeAtividade'],new DateTime($_POST['dataEntrega']),
+$_POST['peso'],$_POST['descricaoAtividade'],$_POST['disciplina']);
+
+$adao = new AtividadeDao();
+
+if($cod!==null){
+    $a->setId(intval($cod));
+    $adao->update($a);
+}
+else{
+    $adao->insert($a);
+}
 
 
+header('Location: ../views/home.php');
+/*
 $a = new Atividade($_POST['nomeAtividade'],new DateTime($_POST['dataEntrega']),
 $_POST['peso'],$_POST['disciplina'],$_POST['descricaoAtividade']);
-//$a = new Atividade('rolosso',new DateTime('12/12/2018'), 5,'artes','tem nada pra ver aqui');
+$adao = new AtividadeDao();
 
-
-//$d=new DateTime(['12/12/2018']);
-//$a->setDataEntrega($d);
-
-
-//print_r($a);
 
 
 
 $adao= new atividadeDao();
 
 $adao->insert($a);
-header('Location: ../views/home.php');
-
+*/
 
 
 
